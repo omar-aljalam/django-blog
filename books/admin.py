1f3d956d4f7e192a61d4ea4d502bfe5d337cf878
Omar <omar-aljalam@outlook.com>
2025-10-23 10:45:08 +0300

    Admin!

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",) # makes the slug field read-only in the admin interface
+    prepopulated_fields = {"slug": ("title",)} # automatically fills the slug field based on the title field
+    list_filter = ("rating", "author", "is_bestseller")
+    list_display = ("title", "author", "rating", "is_bestseller")
+
+admin.site.register(Book, BookAdmin)
