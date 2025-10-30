cf4543b510c37e928d687761b1bcefa18c15f594
Omar <omar-aljalam@outlook.com>
2025-10-30 20:39:41 +0300

    Handle blog views from the db. Add tags, Email author on click

--- books/views.py
+++ books/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
 
 def index(request):
     books = Book.objects.all().order_by("title")
+    avg_rating = books.aggregate(Avg("rating"))
     return render(request, "books/index.html", {
         "books": books,
+        "num_books": books.count(),
+        "avg_rating": avg_rating
 
     })
 
