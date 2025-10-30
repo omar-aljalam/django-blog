3638303eba9ccb5e281ad2af529c017b6e991a2c
Omar <omar-aljalam@outlook.com>
2025-10-30 19:25:27 +0300

    Adding models to the blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+class postAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "date")
+    list_filter = ("author", "date", "tags")
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, postAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
